06bfddfc7d0dfba6ee9c7ab03c0ec2cf71e99399
Aayush Kasera <aayushksr@gmail.com>
2021-10-25 11:50:45 +0530

    Python program
    q

---
+++ Practice.py
@@ -0,0 +1,13 @@
+from random import *
+from pprint import pprint
+lst=[]
+lst.append(123)
+lst.append("122")
+lst.extend("abcd")
+lst.extend(map(int,"123"))
+print(lst)
+lst.append("122")
+print(lst)
+a=list(choice(lst) for i in range(10))
+print(a)
+print(lst.index("122"))
